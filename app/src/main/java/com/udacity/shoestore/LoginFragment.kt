7e59611b46c6fe01f5138a374be9fc6fdcdb4681
tnanhd <tnanh.dev@gmail.com>
2024-08-20 10:57:16 +0700

    Feat: Add welcome and instruction fragments

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -16,7 +17,12 @@ class LoginFragment : Fragment() {
     ): View {
         val binding: FragmentLoginBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
-
+        binding.loginButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
+        )
+        binding.signupButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
+        )
         return binding.root
     }
 }
