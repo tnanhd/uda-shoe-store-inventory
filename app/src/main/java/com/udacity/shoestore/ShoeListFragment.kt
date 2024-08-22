2ee8b3d70f6701b654cacab0b85439caa0f25395
tnanhd <tnanh.dev@gmail.com>
2024-08-22 21:16:50 +0700

    Feat: Add logout menu

--- app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -2,11 +2,17 @@ package com.udacity.shoestore
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 import com.udacity.shoestore.databinding.ShoeItemBinding
 import com.udacity.shoestore.viewmodel.ShoeListViewModel
@@ -40,6 +46,30 @@ class ShoeListFragment : Fragment() {
         )
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuProvider = object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_logout, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.action_logout -> {
+                        view.findNavController()
+                            .navigate(R.id.action_shoeListFragment_to_loginFragment)
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+        }
+        requireActivity().addMenuProvider(menuProvider, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
 }
